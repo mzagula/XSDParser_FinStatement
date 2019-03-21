fcdbb100404aba6d5dca548d47bda7d4f81154e7
Marta Zaguła <mzagula1992@gmail.com>
2019-03-21 16:28:42 +0100

    Add files via upload
    Create path to childs

--- xsdParser.py
+++ xsdParser.py
@@ -18,8 +18,22 @@ def takeParentList(xsd_element):
             parent_element=''
     
     return lista_parent
-            
 
+
+def createChildPath(ls_parent,parent_additional,tag_name,raport_name,txt):
+    list_parent=''
+    child_path=''
+    
+    for i_parent in ls_parent:
+        list_parent= '->' + i_parent + list_parent
+        
+    if tag_name=='PozycjaUszczegolawiajaca':
+        list_parent=  list_parent + '->' +  parent_additional
+        
+    child_path=raport_name  + list_parent + '->' +  tag_name + '->' + txt
+    
+    return child_path
+            
 input_file=fd.askopenfilename()  
 with open(input_file, 'r',encoding='utf-8') as file:
     parser=BeautifulSoup(file, 'html.parser')
@@ -31,7 +45,7 @@ len_parent_prev=0
 pattern_PozUsz=re.compile('(PozycjaUszczegolawiajaca.*)')
 raport=''
 complexType=parser.find_all(re.compile('(xsd:complextype.*)'), attrs={"name":True})
-col=['desc','tag_name','parent', 'level']
+col=['desc','tag_name','parent', 'level', 'path_to_child']
 level=0
 df=pd.DataFrame(columns=col)
 
@@ -48,31 +62,46 @@ for raport in complexType:
         except TypeError:
             parent=complexType_name      
         
-        
-        len_parent=len(takeParentList(i))
-        
+        ls_parent=takeParentList(i)
+        len_parent=len(ls_parent)
         try:
             desc=i.find('xsd:documentation').string
         except AttributeError:
             desc=''
-        
-        
-        if pattern_PozUsz.match(tag_name):
-            lsl.append(['',tag_name,parent,len_parent])
-        else:
-            lsl.append([desc,tag_name,parent,len_parent])
+            
+        if tag_name=='XXII':
+            print("Stop1!")    
+            
+        if str(tag_name)=='XXII':
+            print("Stop2!")      
     
+        child_path=createChildPath(ls_parent,parent, tag_name,raport_name,'{0}')
+        
+        #Pozycja_Uszczegolawiajaca_X
+        if pattern_PozUsz.match(tag_name):
+            lsl.append(['',tag_name,parent,len_parent,child_path])
+        else:
+            lsl.append([desc,tag_name,parent,len_parent,child_path])
+    
+        #Pozycja_Uszczegolawiajaca    
+        
         try:
             if i['type']=='dtsf:TPozycjaSprawozdania' or i['type']=='dtsf:TPozycjaSprawozdaniaTys':
                 parent=tag_name
                 tag_name='PozycjaUszczegolawiajaca'  
                 len_parent=len_parent_prev+1
-                lsl.append(['',tag_name,parent,len_parent])
+                child_path=createChildPath(ls_parent,parent, tag_name,raport_name,'{0}')
+                
+                lsl.append(['',tag_name,parent,len_parent,child_path])
 
+        #Podpozycja    
             if i['type']=='dtsf:TPozycjaSprawozdania' or i['type']=='dtsf:TKwotyPozycjiSprawozdania' or i['type'] == 'dtsf:TKwotyPozycjiSprawozdaniaTys' or i['type']=='dtsf:TPozycjaSprawozdaniaTys':
-                lsl.append(['','Podpozycja',tag_name + ' - ' + parent,len_parent+1])
+                child_path=createChildPath(ls_parent,parent, tag_name,raport_name,'{0}->Podpozycja->{0}')
+                lsl.append(['','Podpozycja',tag_name + ' - ' + parent,len_parent+1,child_path])
+                
         except KeyError:
             pass
+    
         
         len_parent_prev=len_parent
         df=pd.DataFrame(lsl, columns=col)   
